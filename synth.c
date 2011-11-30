748b61f1e9e40a66d1491c15b597d6e695556996
jimee <jimee@votez-cthulhu.net>
2011-11-30 23:22:25 +0100

    Added some exponential stuff, in ADSR and Osc-Volumes.

--- synth.c
+++ synth.c
@@ -3,6 +3,7 @@
 
 #define HALFTONE 1.059463094f
 #define NOTES 32
+#define MIN_VOLUME (1.f / 128.f)
 
 static unsigned int my_seed = 32627;
 
@@ -12,9 +13,18 @@ __attribute__((fastcall)) static float my_rand()
   return ((float)my_seed) / (float)0x80000000;
 }
 
+__attribute((fastcall)) static float my_pow(float x, float y){
+  float res;
+  asm("flds %2; flds %1; fyl2x; fld %%st; frndint; fsubr %%st,%%st(1); fxch %%st(1); f2xm1;"
+      "fld1; faddp; fscale; fstps %0; fstp %%st;"
+      : "=m" (res) 
+      : "m" (x), "m" (y) 
+      : "eax");
+  return res;
+}
+
 __attribute((fastcall)) static float calc_freq(float base, float interval, int nint){
-  while(nint--) base *= interval;
-  return base;
+  return base * my_pow(interval, (float) nint);
 }
 
 /* sin(2 * PI * f) */
@@ -35,7 +45,7 @@ __attribute__((fastcall)) static void create_osc(osc *o, osc_type t, float f, fl
   o->osc  = osc_table[t];
   o->amp  = a;
   o->ph   = 0.f;
-  o->iph  = f / SAMPLERATE_F;
+  o->iph  = f * DT;
 }
 
 __attribute__((fastcall)) static float do_osc(osc *o){
@@ -44,23 +54,23 @@ __attribute__((fastcall)) static float do_osc(osc *o){
 }
 
 __attribute__((fastcall)) static void create_adsr(enveloppe *e, float a, float d, float s, float r){
-  e->v = 0.f;
+  e->v = MIN_VOLUME;
   e->t = 0.f;
   e->a = a;
   e->s = s;
   e->ad = a+d;
-  e->da = (a > 0.f) ? 1.f / (a * SAMPLERATE_F) : 1.f;
-  e->dd = (d > 0.f) ? (1.f - s) / (d * SAMPLERATE_F) : 1.f;
-  e->dr = (r > 0.f) ? s / (r * SAMPLERATE_F) : 1.f;
+  e->da = (a > 0.f) ? my_pow(1.f / MIN_VOLUME, DT / a) : 1.f / MIN_VOLUME;
+  e->dd = (d > 0.f) ? my_pow(s, DT / d) : s;
+  e->dr = (r > 0.f) ? my_pow(MIN_VOLUME / s, DT / r) : 0.f;
   ++e->on;
 }
 
 __attribute__((fastcall)) static float do_adsr(enveloppe *e){
-  if(e->t < e->a) e->v += e->da;
+  if(e->t < e->a) e->v *= e->da;
   else if((e->t - e->a) < DT) e->v = 1.f;
-  else if(e->t < e->ad) e->v -= e->dd;
+  else if(e->t < e->ad) e->v *= e->dd;
   else if(e->on) e->v = e->s;
-  else e->v -= e->dr;
+  else e->v *= e->dr;
   e->t += DT;
   return e->v;
 }
@@ -68,22 +78,16 @@ __attribute__((fastcall)) static float do_adsr(enveloppe *e){
 __attribute__((fastcall)) void create_note_instr(note_instr *n, instrument *i, int f, int a){
   ++n->used;
   n->instr = i;
-  //create_adsr(&(n->env),
-  //            calc_freq(3.258886363e-3f,HALFTONE,i->a),
-  //            calc_freq(3.258886363e-3f,HALFTONE,i->d),
-  //            (float)(i->s) / 127.f,
-  //            calc_freq(3.258886363e-3f,HALFTONE,i->r));
   create_adsr(&(n->env),
               (float) i->a / 127.f,
               (float) i->d / 127.f,
-              (float) i->s / 127.f,
-              (float) i->r / 127.f);
+              my_pow(MIN_VOLUME, 1.f - (float) i->s / 127.f),
+              (float) i->r / 16.f);
   n->freq = f;
   n->amp = (float)(a) / 127.f;
   register int os = 2; do create_osc(n->o+os, i->o[os].type, 
-                        calc_freq(0.405570987f,HALFTONE,i->o[os].freqt+f) * 
-                        calc_freq(0.971531941f,1.000451664f,i->o[os].freqf),
-                        (float)(i->o[os].amp) / 127.f); while(os--);
+                        calc_freq(0.197012587, 1.00045137f, 128 * (i->o[os].freqt + f) + i->o[os].freqf),
+                        my_pow(MIN_VOLUME, 1.f - (float)(i->o[os].amp) / 127.f)); while(os--);
   n->low = n->band = 0.f;
 }
 
@@ -91,14 +95,14 @@ __attribute__((fastcall)) float do_note_instr(note_instr *n){
   float e;
   float out = 0.f;
   if(n->used){
-    if((e = do_adsr(&(n->env)))<=0.f){
+    if((e = do_adsr(&(n->env))) <= MIN_VOLUME){
       --n->used;
     } else {
       int i=3;
       while(i--) out += do_osc(n->o+i);
       out *= e * n->amp;
       n->cutoff = calc_freq(16.f, HALFTONE, n->instr->cutoff);
-      n->f = 1.5f * sin4k(n->cutoff / SAMPLERATE_F / 2.0f);
+      n->f = 1.5f * sin4k(n->cutoff * DT / 2.0f);
       n->res = (float)(n->instr->res) / 127.f;
       n->low += n->f * n->band;
       float high = n->res * (out - n->band) - n->low;
